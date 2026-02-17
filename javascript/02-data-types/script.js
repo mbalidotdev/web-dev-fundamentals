2ed50a7d35d319016aafb07119028aaacd482715
mbalimade-it <mbalideveloper@gmail.com>
2026-02-17 11:52:37 +0100

    Add data types code

---
+++ javascript/02-data-types/script.js
@@ -0,0 +1,22 @@
+/* declare a variable called isIsland. The variable should hold a Boolean value.  */
+
+let isIsland = true;
+
+/* declare a variable language, but don't assign it any value. */
+
+let language;
+
+/* decalre a variable called name, and use your name as a value */
+
+let firstName = "Mbali";
+
+/* declare your age */
+
+let age = 31;
+
+/*  log the types of isIsland, population, country and language to the console. */
+
+console.log(typeof isIsland);
+console.log(typeof language);
+console.log(typeof firstName);
+console.log(typeof age);
