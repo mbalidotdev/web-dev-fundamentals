32b65af50db6ef45542843d30f75347a5ed72f54
mbalimade-it <mbalideveloper@gmail.com>
2026-02-17 14:45:10 +0100

    Add strings template literals file

---
+++ javascript/03-strings-template-literals/script.js
@@ -0,0 +1,13 @@
+let country = "Portugal";
+let continent = "Europe";
+let population = 10;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// recreate the fullMessage variable using the template literal syntax.
+
+let fullMessage = `${country} is based in ${continent} with over ${population} million people.`;
+
+console.log(fullMessage);
