6870961bdc041b7e3dd69dd1dca4d3751a10a77c
mbalimade-it <mbalideveloper@gmail.com>
2026-02-17 11:40:40 +0100

    Add values and variables learning files

---
+++ javascript/01-values-and-variables/script.js
@@ -0,0 +1,9 @@
+// decalre variables country, continent, population
+let country = "Portugal";
+let continent = "Europe";
+let population = 10;
+
+// log values to the console
+console.log(country);
+console.log(continent);
+console.log(population);
